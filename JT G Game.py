841c7ba89ae90ae43824060369cd110a29f58d35
Abblebees <45661872+Abblebees@users.noreply.github.com>
2018-12-11 08:11:33 -0600

    Update JT G Game.py

--- JT G Game.py
+++ JT G Game.py
@@ -11,9 +11,27 @@ maze = [[a,a,a,a,a,a,b,a],
         [a,a,a,b,b,a,b,a],
         [a,b,b,b,b,b,b,a],
         [a,b,a,a,a,a,a,a]]
+
+game_over = False
+
 MC = (254,254,254)
 x = 6
 y = 0
-maze[y][x] = MC
 
-sense.set_pixels(sum(maze,[]))
+while game_over == False:
+    o = sense.get_orientation()
+    pitch = o["pitch"]
+    roll = o["roll"]
+    x,y = move_marble(pitch,roll,x,y)
+    maze[y][x] = MC
+    sense.set_pixels(sum(maze,[]))
+
+def move_marble(pitch, roll, x, y):
+	new_x = x
+	new_y = y
+	if 1 < pitch < 179:
+            new_x -= 1
+        if 181 < pitch < 359:
+            new_x += 1
+   
+
