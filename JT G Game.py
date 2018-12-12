acde7d1afc2b30a8d4b0ae628d83e8ddc49761a9
Abblebees <45661872+Abblebees@users.noreply.github.com>
2018-12-12 09:00:48 -0600

    Update JT G Game.py

--- JT G Game.py
+++ JT G Game.py
@@ -1,6 +1,8 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.clear()
+from time import sleep
+
 a = (0,110,90)
 b = (0,0,0)
 maze = [[a,a,a,a,a,a,b,a],
@@ -18,6 +20,14 @@ MC = (254,254,254)
 x = 6
 y = 0
 
+def move_marble(pitch, roll, x, y):
+    new_x = x
+    new_y = y
+    if 1 < pitch < 179:
+            new_x -= 1
+    if 181 < pitch < 359:
+            new_x += 1
+
 while game_over == False:
     o = sense.get_orientation()
     pitch = o["pitch"]
@@ -25,13 +35,6 @@ while game_over == False:
     x,y = move_marble(pitch,roll,x,y)
     maze[y][x] = MC
     sense.set_pixels(sum(maze,[]))
-
-def move_marble(pitch, roll, x, y):
-	new_x = x
-	new_y = y
-	if 1 < pitch < 179:
-            new_x -= 1
-        if 181 < pitch < 359:
-            new_x += 1
-   
-
+    sleep(0.05)
+    maze[y][x] = b
+    
